b85868b66def1a97d1b0e10edb7ffbea5767380a
Ivny Metzker <ivnymetzker@gmail.com>
2023-02-09 20:11:24 -0300

    Style - Home
    Adicionando estrutura, efeitos e estilos

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const textIntro = document.querySelector(".intro-content h1");
+
+function typeWriter(el) {
+  const textArray = el.innerHTML.split("");
+  el.innerHTML = "";
+
+  textArray.forEach((letra, i) => {
+    setTimeout(() => {
+      el.innerHTML += letra;
+    }, 75 * i);
+  });
+}
+
+typeWriter(textIntro);
