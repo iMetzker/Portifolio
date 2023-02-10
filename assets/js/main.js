c3c3e8d9e9cf34d9765c9e7e01097992fca08588
Ivny Metzker <ivnymetzker@gmail.com>
2023-02-10 00:26:55 -0300

    Style - About
    Adicionando estrutura e estilos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//TITLE
 const textIntro = document.querySelector(".intro-content h1");
 
 function typeWriter(el) {
@@ -12,3 +13,18 @@ function typeWriter(el) {
 }
 
 typeWriter(textIntro);
+
+// MENU CLICK
+const blinkSelection = document.querySelectorAll(".blink-arrow");
+
+
+function activeBlink () {
+  blinkSelection.forEach((element) => {
+    element.classList.remove("active");
+  });
+  this.classList.add("active");
+}
+
+blinkSelection.forEach((element) => {
+  element.addEventListener("click", activeBlink);
+});
